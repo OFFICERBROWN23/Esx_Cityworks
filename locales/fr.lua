74398395b01a575e108c985d2e67647e86c96f78
OFFICERBROWN23 <41652622+OFFICERBROWN23@users.noreply.github.com>
2018-11-04 11:56:52 -0500

    Add files via upload

---
+++ locales/fr.lua
@@ -0,0 +1,16 @@
+Locales['fr'] = {
+	['GPS_info'] = 'A new job has been highlighted on your ~y~GPS',
+	['cancel_mission'] = 'You have ~r~cancelled ~s~the mission',
+	['pickup'] = 'Press ~INPUT_CONTEXT~ to inspect the work site',
+	['end_service'] = 'End Service',
+	['take_service'] = 'Take Service',
+	['end_service_notif'] = 'Ended ~r~Service',
+	['take_service_notif'] = 'Started ~g~Service',
+	['start_job'] = 'Press ~b~F10 ~w~to start the job',
+	['Vehicle_Menu_Title'] = 'Service Vehicle',
+	['in_vehicle'] = '~r~You must be out of your vehicle!',
+	['vehicle_broken'] = '~r~You need to repair the vehicle before continuing!',
+	['bad_vehicle'] = 'You can only store the ~b~Service Vehicle',
+	['not_good_veh'] = '~r~You must be in your service vehicle!',
+	['stop_npc'] = 'Job ~g~Complete~s~ : Return to the depot',
+}
