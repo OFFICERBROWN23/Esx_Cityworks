65a2fddbf22e95a66c0823ad26978663c1b3e114
OFFICERBROWN23 <41652622+OFFICERBROWN23@users.noreply.github.com>
2019-05-07 12:37:46 -0400

    Add files via upload

---
+++ esx_cityworks/esx_cityworks.sql
@@ -0,0 +1,8 @@
+INSERT INTO `jobs` (`name`, `label`) VALUES
+('works', 'City Works');
+
+INSERT INTO `job_grades` (`job_name`, `grade`, `name`, `label`, `salary`, `skin_male`, `skin_female`) VALUES
+('works', 0, 'interim', 'Employee', 150, '{}', '{}');
+
+INSERT INTO `items` (`name`, `label`, `limit`, `rare`, `can_remove`) VALUES
+('pay_works', 'City Pay Slip', 15, 0, 1);
